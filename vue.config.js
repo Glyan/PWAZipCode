777ff965adaee9932fb8e9bd5f46dddc097fec67
Glyan <pamela27jr@gmail.com>
2020-08-06 17:04:38 +0100

    PWA finished

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
     pwa: {
-        workboxPluginMode: 'GenerateSW'
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            navigateFallback: '/index.html',
+            runtimeCaching: [
+                {
+                    urlPattern: new RegExp('^https://api.zippopotam.us/us/'),
+                    handler: 'networkFirst',
+                    options: {
+                        networkTimeoutSeconds: 20,
+                        cacheName: 'api-cache',
+                        cacheableResponse: {
+                            statuses: [0, 200]
+                        }
+                    }
+                }
+            ]
+        }
     }
 }
